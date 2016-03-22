9ec5611502e1297940de89eb389ef9a74d86ac2e
Faye <fayvado@yahoo.com>
2016-03-22 00:49:27 -0400

    First Test created VendingMachineTest; it is not correct but it is a start

---
+++ VendingMachine/src/edu/towson/cis/cosc603/project4/vendingmachine/VendingMachineTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package edu.towson.cis.cosc603.project4.vendingmachine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author fayvado
+ * The unit test Class for VendingMachine. 
+ */
+public class VendingMachineTest {
+
+	/**  Declaring test objects for {@link VendingMachine}  */
+	
+	VendingMachine vend1, vend2;
+	private VendingMachineItem vendItemArray;
+	
+	
+	
+	/**   Initializes the necessary test objects for the test
+	 *    cases to use
+	 *    
+	 *    @throws Exception the exception
+	 */
+	
+	@Test 
+	public void testVendingMachine() {
+		assertEquals(null,vendItemArray);
+			
+	}
+	
+	
+	
+	
+}
